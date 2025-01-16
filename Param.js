f169c43700c851e0c48f3e414df0b91f4928c99e
Ujjwal Aggarwal <aggarwal.ujjwal05@gmail.com>
2025-01-16 11:22:19 +0530

    ES6

---
+++ Param.js
@@ -0,0 +1,11 @@
+function classpractice (a, b = 10,arr1,arr2) { 
+    console.log(((a * b)/100));
+    console.log(...arr1, ...arr2);
+    var a = {
+        name: "ujjwal",
+        Section: "k22LL",
+    };
+    console.log(a);
+
+}
+classpractice(100, 20, [1, 2, 3, 400], [500, 600, 700, 800]);
